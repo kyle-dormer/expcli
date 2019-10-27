7a1ef8b5e1f62caa0296e11053f18695aa8e54e8
Kyle Dormer <kyle.dormer2000@gmail.com>
2019-10-27 15:43:15 +0000

    Add main file and expcli module

---
+++ __main__.py
@@ -0,0 +1,10 @@
+"""
+Author: Kyle Dormer
+Date: 27/10/2019
+Student Number: s1802423
+"""
+
+from expcli import main
+
+if __name__ == '__main__':
+    main()
---
+++ expcli.py
@@ -0,0 +1,9 @@
+"""
+Author: Kyle Dormer
+Date: 27/10/2019
+Student Number: s1802423
+"""
+
+
+def main():
+    pass
