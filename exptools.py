20c4900233ad3168710df753d5da66266c67a4c0
Kyle Dormer <26012628+kyle-dormer@users.noreply.github.com>
2019-11-08 00:02:25 +0000

    Add module for tools and function for validating user input

---
+++ exptools.py
@@ -0,0 +1,19 @@
+"""
+Author: Kyle Dormer
+Date: 27/10/2019
+Student Number: s1802423
+"""
+
+
+def validate_input(user_input, lower_length, upper_length, desired_type):
+    """
+    user_input (str), lower_length (int), upper_length (int), desired_type
+    (type) -> bool
+    Used for validating user input. It takes the user's input, the lowest
+    acceptable length, the highest acceptable length and the desired type of
+    input. It then returns a boolean according to whether the user's input
+    matches the given length range and type.
+    """
+    return ((len(user_input) >= lower_length) and
+            (len(user_input) <= upper_length)) and \
+        type(user_input) == desired_type
