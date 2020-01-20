55613f01f4f9b558e9e8d9e863969d7ec37b1251
Kyle Dormer <26012628+kyle-dormer@users.noreply.github.com>
2020-01-20 09:14:15 +0000

    Add init method to set up database on load

--- expsql.py
+++ expsql.py
@@ -4,7 +4,20 @@ Date: 05/11/2019
 Student Number: s1802423
 """
 
+import sqlite3
 
-def connect_db():
-    """Open connection to local SQLite database"""
-    pass
+conn = sqlite3.connect('expenses.db')
+c = conn.cursor()
+
+
+def db_init():
+    c.execute('CREATE TABLE IF NOT EXISTS Expense(ID INTEGER NOT NULL PRIMARY \
+                                                  KEY AUTOINCREMENT UNIQUE,   \
+                                                  Date TEXT, Category TEXT,   \
+                                                  Amount REAL)')
+
+    c.execute('CREATE TABLE IF NOT EXISTS Month(Date INTEGER NOT NULL \
+            PRIMARY KEY UNIQUE, Budget REAL)')
+
+    c.execute('CREATE TABLE IF NOT EXISTS Category(Name TEXT NOT NULL PRIMARY \
+    KEY UNIQUE, Budget REAL)')
