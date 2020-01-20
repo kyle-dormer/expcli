355e2eaea4044391cc728985693a3cf68049836b
Kyle Dormer <26012628+kyle-dormer@users.noreply.github.com>
2020-01-20 10:32:02 +0000

    Add method to store expenses in db

--- expsql.py
+++ expsql.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Author: Kyle Dormer
 Date: 05/11/2019
@@ -5,24 +6,35 @@ Student Number: s1802423
 """
 
 import sqlite3
+from datetime import date as d
 
-conn = sqlite3.connect('expenses.db')
-c = conn.cursor()
+connection = sqlite3.connect('expenses.db')
+cursor = connection.cursor()
 
 
 def db_init():
-    c.execute('CREATE TABLE IF NOT EXISTS Expense(ID INTEGER NOT NULL PRIMARY \
+    cursor.execute('CREATE TABLE IF NOT EXISTS Expense(ID INTEGER NOT NULL PRIMARY \
                                                   KEY AUTOINCREMENT UNIQUE,   \
                                                   Date TEXT, Category TEXT,   \
                                                   Amount REAL)')
 
-    c.execute('CREATE TABLE IF NOT EXISTS Month(Date INTEGER NOT NULL \
+    cursor.execute('CREATE TABLE IF NOT EXISTS Month(Date INTEGER NOT NULL \
             PRIMARY KEY UNIQUE, Budget REAL)')
 
-    c.execute('CREATE TABLE IF NOT EXISTS Category(Name TEXT NOT NULL PRIMARY \
+    cursor.execute('CREATE TABLE IF NOT EXISTS Category(Name TEXT NOT NULL PRIMARY \
     KEY UNIQUE, Budget REAL)')
 
 
 def db_close():
-    c.close()
-    conn.close()
+    cursor.close()
+    connection.close()
+
+
+def store_expense(date, category, amount):
+    if date is None:
+        date = d.today()
+
+    cursor.execute(
+        'INSERT INTO Expense(Date, Category, Amount) VALUES (?, ?, ?)', (date, category, amount))
+
+    connection.commit()
