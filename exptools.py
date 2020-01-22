425e55418333dbd1b8f2fd2609fa46063216e7b4
Kyle Dormer <26012628+kyle-dormer@users.noreply.github.com>
2020-01-22 09:05:09 +0000

    Add get_week util

--- exptools.py
+++ exptools.py
@@ -32,3 +32,12 @@ def validate_date(date_string):
 
     except (ValueError):
         return False
+
+
+def get_week(date_string):
+    if validate_date(date_string):
+        try:
+            datetime_object = datetime.strptime(date_string, '%Y-%m-%d')
+            return datetime_object.isocalendar()[1]
+        except (TypeError):
+            return False
